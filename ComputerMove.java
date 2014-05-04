8354f7b283d2f75dfed9d5c410170be615678a6f
cauchyh <cauchy.huang@gmail.com>
2014-05-03 22:47:27 -0400

    Update on AI
    Remove some stupid compile error of the AI file. AI update to be
    continued.

--- ComputerMove.java
+++ ComputerMove.java
@@ -3,13 +3,13 @@
 	received its hand, including split the 7 cards into 5+2
 	And how to identify the 5 cards out of 7. 
 */
-
+import java.util.*;
 public class ComputerMove
 {
-	final static HIGH_HAND_NUMBER = 5;
-	final static LOW_HAND_NUMBER = 2;
-	Poker[] highHand = new Poker[5];
-	Poker[] lowHand = new Poker[2];
+	final static int HIGH_HAND_NUMBER = 5;
+	final static int LOW_HAND_NUMBER = 2;
+	private Poker[] highHand = new Poker[5];
+	private Poker[] lowHand = new Poker[2];
 
 	public ComputerMove(Poker[] cards)
 	{
@@ -22,6 +22,22 @@ public class ComputerMove
 		// Use a naive and sometimes wrong approach first
 		lowHand = Arrays.copyOfRange(cards, 0, LOW_HAND_NUMBER);
 		highHand = Arrays.copyOfRange(cards, LOW_HAND_NUMBER, HIGH_HAND_NUMBER);
+	}
+
+	/**
+	 * @return high hand 5 cards of the 7
+	 */
+	public Poker[] getHighHand()
+	{
+		return highHand;
+	}
+
+	/**
+	 * @return low hand 3 cards of the 7
+	 */
+	public Poker[] getLowHand()
+	{
+		return lowHand;
 	}
 
 }
